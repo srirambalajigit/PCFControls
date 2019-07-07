95c2b92157e4e1e1250f7aa82bbaf91b918a36df
srirambalajigit <51313549+srirambalajigit@users.noreply.github.com>
2019-07-07 22:23:34 +0530

    Add files via upload

---
+++ ColorGrid/ColorGrid/generated/ManifestTypes.d.ts
@@ -0,0 +1,14 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    headerBackgroundColor: ComponentFramework.PropertyTypes.StringProperty;
+    headerForegroundColor: ComponentFramework.PropertyTypes.StringProperty;
+    rowBackColor: ComponentFramework.PropertyTypes.StringProperty;
+    cellBackgroundColor: ComponentFramework.PropertyTypes.StringProperty;
+    simpleTableGrid: ComponentFramework.PropertyTypes.DataSet;
+}
+export interface IOutputs {
+}
